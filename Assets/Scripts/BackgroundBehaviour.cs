89c5b5ef75c5aca88bbd2c67609024eae768fec7
oussamabonnor1 <oussamabonnor@gmail.com>
2019-12-21 19:47:42 +0100

    adding background with infinite scrolling effect

---
+++ Assets/Scripts/BackgroundBehaviour.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundBehaviour : MonoBehaviour {
+
+	Vector2 offset;
+	Vector2 previousPosition;
+	Material backgroundMat;
+
+	public float speed;
+
+	// Use this for initialization
+	void Start () {
+		backgroundMat = GetComponent<Renderer>().material;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		offset = new Vector2(transform.position.x, transform.position.y) - previousPosition;
+		backgroundMat.mainTextureOffset += offset * speed * Time.deltaTime;
+		previousPosition = transform.position;
+	}
+}
