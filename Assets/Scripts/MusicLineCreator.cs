545581ed0c81e5f633d6c94c9d2792727db10d1a
oussamabonnor1 <oussamabonnor@gmail.com>
2019-12-22 20:18:54 +0100

    adding smoothing functions to the generated graph

--- Assets/Scripts/MusicLineCreator.cs
+++ Assets/Scripts/MusicLineCreator.cs
@@ -10,23 +10,29 @@ public class MusicLineCreator : MonoBehaviour {
 	public Rigidbody2D shipRb;
 
 	public float graphMagnitude;
-	public float graphPointX, graphPointY;
+	public float graphPointX, graphPointY, yLerpingSpeed;
 	Vector2 graphPosition;
 
 	public float[] samples = new float[512];
 
 	void Start () {
 		if (shipRb.isKinematic) shipRb.bodyType = RigidbodyType2D.Dynamic;
-			Camera.main.GetComponent<CameraBehaviour> ().cameraFollow ();
+		Camera.main.GetComponent<CameraBehaviour> ().cameraFollow ();
 	}
 
 	void Update () {
-		audioSource.GetSpectrumData(samples,0,FFTWindow.Blackman);
+		audioSource.GetSpectrumData (samples, 0, FFTWindow.Blackman);
 
 		if (activeLine != null) {
-			graphPointY= (samples[0] + samples[1] + samples[2]) * graphMagnitude;
+			float samplesSum = 0;
+			for (int i = 0; i < 4; i++) {
+				samplesSum += samples[i];
+			}
+
+			graphPointY = (samplesSum) * graphMagnitude;
 			graphPointX = Mathf.Max(shipRb.velocity.x, graphPointY);
-			graphPosition = new Vector2 (graphPosition.x + (graphPointX * Time.deltaTime), graphPointY);
+			graphPosition = new Vector2 (graphPosition.x + (graphPointX * Time.deltaTime),
+				Mathf.Lerp (graphPosition.y, graphPointY, yLerpingSpeed * Time.deltaTime));
 			activeLine.updateLine (graphPosition);
 		}
 	}
