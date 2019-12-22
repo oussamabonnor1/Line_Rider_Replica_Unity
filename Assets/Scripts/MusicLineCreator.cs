ee16eef81cf8452c671583b2aaeba171c0ede5f4
oussamabonnor1 <oussamabonnor@gmail.com>
2019-12-22 18:54:57 +0100

    adding music visualization scene

---
+++ Assets/Scripts/MusicLineCreator.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MusicLineCreator : MonoBehaviour {
+
+	public GameObject linePrefab;
+	public LineBehaviour activeLine;
+	public Rigidbody2D shipRb;
+
+	void Update () {
+		if (Input.GetMouseButtonDown (0)) {
+			GameObject line = Instantiate (linePrefab);
+			activeLine = line.GetComponent<LineBehaviour> ();
+		}
+
+		if (activeLine != null) {
+			Vector2 mousePosition = Camera.main.ScreenToWorldPoint (Input.mousePosition);
+			activeLine.updateLine (mousePosition);
+		}
+
+		if (Input.GetMouseButtonUp (0)){ 
+			activeLine = null;
+			Camera.main.GetComponent<CameraBehaviour>().cameraFollow();
+			if(shipRb.isKinematic) shipRb.bodyType = RigidbodyType2D.Dynamic;
+		}
+	}
+
+
+	public void restartScene(){
+		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+	}
+}
